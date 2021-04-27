5a5d348f9e26629e86d72f81c22f7f06888509da
lionel <massou.lionel@gmail.com>
2021-04-27 17:26:52 +0200

    essai du point 4

--- src/Table.js
+++ src/Table.js
@@ -5,20 +5,6 @@ import Button from './componant/Button.jsx'
 import Cartes from "./componant/Cartes";
 import Game from './componant/Game.jsx'
 
-const cardArray = [
-  "KS", "QS", "JS", "AS", "2S", "3S", "4S", "5S", "6S", "7S", "8S", "9S", "0S",
-  "KD", "QD", "JD", "AD", "2D", "3D", "4D", "5D", "6D", "7D", "8D", "9D", "0D",
-  "KH", "QH", "JH", "AH", "2H", "3H", "4H", "5H", "6H", "7H", "8H", "9H", "0H",
-  "KC", "QC", "JC", "AC", "2C", "3C", "4C", "5C", "6C", "7C", "8C", "9C", "0C"];
-
-// const min = 0
-// const cardCount = 52
-
-// let rndNum = 0
-// let temp = ""
-let arrayLength = 0
-let rndCarteTemp = "";
-let rndNumTemp = 0;
 class Table extends React.Component {
   constructor() {
     super();
@@ -29,22 +15,33 @@ class Table extends React.Component {
       playerCardList: [],
       dealerCardList: [],
       startGame: false,
-      premierLance: "yes",
+      // premierLance: "yes",
       endGame: false,
       nameOfWinner: ""
     }
   }
 
+  cardArray = [
+    "KS", "QS", "JS", "AS", "2S", "3S", "4S", "5S", "6S", "7S", "8S", "9S", "0S",
+    "KD", "QD", "JD", "AD", "2D", "3D", "4D", "5D", "6D", "7D", "8D", "9D", "0D",
+    "KH", "QH", "JH", "AH", "2H", "3H", "4H", "5H", "6H", "7H", "8H", "9H", "0H",
+    "KC", "QC", "JC", "AC", "2C", "3C", "4C", "5C", "6C", "7C", "8C", "9C", "0C"];
+
+  arrayLength = 0;
+  rndCarteTemp = "";
+  rndNumTemp = 0;
+
   rndCarte() {
-    arrayLength = + this.state.playerCardList.length;
+    this.arrayLength = + this.state.playerCardList.length;
 
-    rndNumTemp = Math.floor(Math.random() * 53);
+    this.rndNumTemp = Math.floor(Math.random() * 53);
 
-    if (rndNumTemp > 52) { rndNumTemp = rndNumTemp - 10 } else if (rndNumTemp < 1) { rndNumTemp = rndNumTemp + 10 }
+    if (this.rndNumTemp > 52) { this.rndNumTemp = this.rndNumTemp - 10 } 
+    else if (this.rndNumTemp < 1) { this.rndNumTemp = this.rndNumTemp + 10 }
 
-    rndCarteTemp = cardArray[rndNumTemp - 1];
+    this.rndCarteTemp = this.cardArray[this.rndNumTemp - 1];
 
-    return rndCarteTemp
+    return this.rndCarteTemp
   }
 
   onClickStop = () => {
@@ -147,7 +144,7 @@ class Table extends React.Component {
   }
 
   render() {
-    if (this.state.startGame == false) {
+    if (this.state.startGame === false) {
       return (
         <Game startGame={this.startGame} />
       )
